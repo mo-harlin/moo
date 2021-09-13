94c3fbb4d7c6ff8a45fad3a940b3e1e1900215f0
mo-harlin <90276811+mo-harlin@users.noreply.github.com>
2021-09-13 17:29:08 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -6247,12 +6247,12 @@ return false
 end
 tdcli_function ({ID = "GetUserProfilePhotos",user_id_ = owner_id,offset_ = 0,limit_ = 1},function(arg,getpro) 
 if getpro.photos_[0] then
-Name = '• المنشئ ➥ ['..b.first_name_..'](tg://user?id='..b.id_..')\n 
-Name = 'Name..' • البايو ➥ ['..getbio(owner_id)..']\n 
+Name = "• المنشئ ➥ ["..b.first_name_.."](tg://user?id="..b.id_..")\n 
+Name = "Name.." • البايو ➥ ["..getbio(owner_id).."]\n 
 keyboard = {}
-keyboard.inline_keyboard = {{{text =  '..b.first_name_.. ', url = "https://t.me/"..b.username_..""}},}
+keyboard.inline_keyboard = {{{text =  '..b.first_name_..' ', url = "https://t.me/"..b.username_..""}},}
 local msg_id = msg.id_/2097152/0.5
-https.request("https://api.telegram.org/bot"..token.."/sendPhoto?chat_id='..msg.chat_id_..'&caption='..URL.escape(Name)..'&photo='..getpro.photos_[0].sizes_[1].photo_.persistent_id_..'&reply_to_message_id='..msg_id..'&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
+https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/'..UserName..'&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
 else
 
 send(msg.chat_id_, msg.id_,Name,1,  md )

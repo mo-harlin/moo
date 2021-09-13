ff1d28f8a1751ae10f223679244b2730cde56205
mo-harlin <90276811+mo-harlin@users.noreply.github.com>
2021-09-13 18:02:07 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -3164,17 +3164,15 @@ local Text = [[
 keyboard = {} 
 keyboard.inline_keyboard = {
 {{text = '𝙱𝙾𝙾𝙳𝚈',url="t.me/BoOdY_BaSha"},{text = '𝙱𝙾𝙾𝙳𝚈',url="t.me/BoOdY_BaSha"}},
+{
 {{text = '𝙱𝙾𝙾𝙳𝚈', url="t.me/BoOdY_BaSha"}},
+},
 {{text = '𝙱𝙾𝙾𝙳𝚈',url="t.me/BoOdY_BaSha"},{text = '𝙱𝙾𝙾𝙳𝚈',url="t.me/BoOdY_BaSha"}},
 }
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/BoOdY_BaSha&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end
-{{text = '𝙱𝙾𝙾𝙳𝚈',url="t.me/BoOdY_BaSha"},{text = '𝙱𝙾𝙾𝙳𝚈',url="t.me/BoOdY_BaSha"}},
-{
-{{text = '𝙱𝙾𝙾𝙳𝚈', url="t.me/BoOdY_BaSha"}},
-},
-{{text = '𝙱𝙾𝙾𝙳𝚈',url="t.me/BoOdY_BaSha"},{text = '𝙱𝙾𝙾𝙳𝚈',url="t.me/BoOdY_BaSha"}},
+
 if text == 'مارتن' or text == 'توام هارلين' or text == 'مبرمج السورس' then
 local Text = [[
 مارتن مبرمج سورس نوفا لوحابب تتواصل معاه ❤️.
